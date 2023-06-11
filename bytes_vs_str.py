13723ff841701468918487eee71fd020db10bfd9
Mahtab Syed <mahtab.syed@gmail.com>
2023-06-11 12:01:16 +1000

    Beginners section

---
+++ bytes_vs_str.py
@@ -0,0 +1,9 @@
+print("------")
+a = b"Hello"
+print(list(a))
+print(a)
+
+print("------")
+a = "a\u0300 propos"
+print(list(a))
+print(a)
---
+++ decorators.py
@@ -0,0 +1,5 @@
+# Decorators and Context Managers
+# Helps separate business logic from administration logic
+# Clean, beautiful tools for factoring code improving code reuse
+# Good naming is essential
+# Remember the Spiderman rule: With great power comes great responsibility!
